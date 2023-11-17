22f41419fe928bbebedcd4a0f388007d030cdb4d
rakhagans37 <muhammadrakha3704@gmail.com>
2023-11-17 23:27:37 +0700

    Perubahan file validate login : membuat variabel untuk menyimpan editorId dan editorProfilePhoto

--- connection/validateLoginEditor.php
+++ connection/validateLoginEditor.php
@@ -8,3 +8,9 @@ if (!isset($_COOKIE['editorLoginStatus']) && !isset($_SESSION['editorLoginStatus
     header('Location:loginEditor.php');
     exit;
 }
+
+//Get Photo Profile
+$editorProfilePhoto = $_COOKIE['editorProfilePhoto'] ?? $_SESSION['editorProfilePhoto'];
+
+//Get Admin ID
+$editorId = $_SESSION['idAdmin'] ?? $_COOKIE['idAdmin'];
