b47232c2350fd1f8a9e63b026d9695a958616702
rakhagans37 <muhammadrakha3704@gmail.com>
2023-11-25 00:15:12 +0700

    Menambah redirect detail

--- reader/listBlog.php
+++ reader/listBlog.php
@@ -1,28 +1,26 @@
 <?php
 include('../helper/getConnectionMsqli.php');
-require_once __DIR__ . '/../helper/cloudinary.php';
-require_once __DIR__ . '/../helper/hash.php';
 
 $conn = getConnectionMysqli();
 
 $getCategory = isset($_GET['category']) ? $_GET['category'] : null;
 
 if (is_null($getCategory)) {
-	$query = "SELECT tb_event.event_id, tb_event.event_title, tb_event.date_release, tb_event.image_url, tb_editor.username, tb_category_event.category_name FROM tb_event INNER JOIN tb_editor ON tb_event.editor_id = tb_editor.editor_id INNER JOIN  tb_category_event ON tb_category_event.category_id = tb_event.category_id";
+	$query = "SELECT tb_blog.blog_id, tb_blog.blog_title, tb_blog.date_release, tb_blog.image_url, tb_editor.username, tb_category_blog.category_name FROM tb_blog INNER JOIN tb_editor ON tb_blog.editor_id = tb_editor.editor_id INNER JOIN  tb_category_blog ON tb_category_blog.category_id = tb_blog.category_id";
 } else {
-	$query = "SELECT tb_event.event_id, tb_event.event_title, tb_event.date_release, tb_event.image_url, tb_editor.username, tb_category_event.category_name FROM tb_event INNER JOIN tb_editor ON tb_event.editor_id = tb_editor.editor_id INNER JOIN  tb_category_event ON tb_category_event.category_id = tb_event.category_id WHERE tb_category_event.category_name = '$getCategory'";
+	$query = "SELECT tb_blog.blog_id, tb_blog.blog_title, tb_blog.date_release, tb_blog.image_url, tb_editor.username, tb_category_blog.category_name FROM tb_blog INNER JOIN tb_editor ON tb_blog.editor_id = tb_editor.editor_id INNER JOIN  tb_category_blog ON tb_category_blog.category_id = tb_blog.category_id WHERE tb_category_blog.category_name = '$getCategory'";
 }
 
 $data = mysqli_query($conn, $query);
 $result = mysqli_fetch_all($data);
-$query2 = "SELECT tb_event.category_id, tb_category_event.category_name, COUNT(tb_event.category_id) AS jumlah_kategori FROM tb_category_event INNER JOIN tb_event ON tb_category_event.category_id = tb_event.category_id GROUP BY tb_event.category_id";
-$query3 = "SELECT COUNT(event_id) AS jumlah_event FROM tb_event";
+$query2 = "SELECT tb_blog.category_id, tb_category_blog.category_name, COUNT(tb_blog.category_id) AS jumlah_kategori FROM tb_category_blog INNER JOIN tb_blog ON tb_category_blog.category_id = tb_blog.category_id GROUP BY tb_blog.category_id";
+$query3 = "SELECT COUNT(blog_id) AS jumlah_blog FROM tb_blog";
 $data2 = mysqli_query($conn, $query2);
 $result2 = mysqli_fetch_all($data2);
 $data3 = mysqli_query($conn, $query3);
 $result3 = mysqli_fetch_array($data3);
 
-$query4 = "SELECT event_id, event_title, date_release, views, image_url FROM tb_event ORDER BY views desc limit 3";
+$query4 = "SELECT blog_id, blog_title, date_release, views, image_url FROM tb_blog ORDER BY views desc limit 3";
 $data4 = mysqli_query($conn, $query4);
 $result4 = mysqli_fetch_all($data4);
 ?>
@@ -53,7 +51,7 @@ $result4 = mysqli_fetch_all($data4);
 
 <body>
 
-	<!-- preloader
+	<!-- preloader -->
 	<div id="preloader">
 		<div class="book">
 			<div class="inner">
@@ -82,7 +80,7 @@ $result4 = mysqli_fetch_all($data4);
 				<li></li>
 			</ul>
 		</div>
-	</div> -->
+	</div>
 
 	<!-- site wrapper -->
 	<div class="site-wrapper">
@@ -132,10 +130,10 @@ $result4 = mysqli_fetch_all($data4);
 							<li class="nav-item">
 								<a class="nav-link" href="index.php">Home</a>
 							</li>
-							<li class="nav-item active">
+							<li class="nav-item">
 								<a class="nav-link" href="listEvent.php">Event</a>
 							</li>
-							<li class="nav-item">
+							<li class="nav-item active">
 								<a class="nav-link" href="listBlog.php">Blog</a>
 							</li>
 							<li class="nav-item">
@@ -154,11 +152,11 @@ $result4 = mysqli_fetch_all($data4);
 		<section class="page-header">
 			<div class="container-xl">
 				<div class="text-center">
-					<h1 class="mt-0 mb-2">Event</h1>
+					<h1 class="mt-0 mb-2">Blog</h1>
 					<nav aria-label="breadcrumb">
 						<ol class="breadcrumb justify-content-center mb-0">
 							<li class="breadcrumb-item"><a href="#">Home</a></li>
-							<li class="breadcrumb-item active" aria-current="page">Event</li>
+							<li class="breadcrumb-item active" aria-current="page">Blog</li>
 						</ol>
 					</nav>
 				</div>
@@ -177,10 +175,10 @@ $result4 = mysqli_fetch_all($data4);
 							<?php
 							if (mysqli_num_rows($data) > 0) {
 								foreach ($result as $isi) {
-									$eventId = $isi[0];
-									$eventTitle = $isi[1];
+									$blogId = $isi[0];
+									$blogTitle = $isi[1];
 									$dateRelease = $isi[2];
-									$image = getImageDefault(decryptPhotoProfile($isi[3]));
+									$image = $isi[3];
 									$editorUsername = $isi[4];
 									$categoryName = $isi[5];
 
@@ -192,16 +190,16 @@ $result4 = mysqli_fetch_all($data4);
 													<a href="category.html" class="category-badge position-absolute">$categoryName</a>
 													<a href="blog-single.html">
 														<div class="inner">
-															<img src="$image" alt="post-title" />
+															<img src="images/posts/post-md-2.jpg" alt="post-title" />
 														</div>
 													</a>
 												</div>
 												<div class="details">
 													<ul class="meta list-inline mb-0">
-														<li class="list-inline-item"><a href="#">$editorUsername</a></li>
+														<li class="list-inline-item"><a href="detailBlog.php?blogId=$blogId">$editorUsername</a></li>
 														<li class="list-inline-item">$dateRelease</li>
 													</ul>
-													<h5 class="post-title mb-3 mt-3"><a href="detailEvent.php?eventId=$eventId">$eventTitle</a></h5>
+													<h5 class="post-title mb-3 mt-3"><a href="detailBlog.php?blogId=$blogId">$blogTitle</a></h5>
 												</div>
 												<div class="post-bottom clearfix d-flex align-items-center">
 													<div class="social-share me-auto">
@@ -255,23 +253,23 @@ $result4 = mysqli_fetch_all($data4);
 									<?php
 									$number = 1;
 									foreach ($result4 as $data) {
-										$popularEventId = $data[0];
-										$popularEventTitle = $data[1];
-										$popularEventDate = $data[2];
+										$popularBlogId = $data[0];
+										$popularBlogTitle = $data[1];
+										$popularBlogDate = $data[2];
 										echo <<<Buat
 											<div class="post post-list-sm circle">
 											<div class="thumb circle">
 												<span class="number">$number</span>
-												<a href="detailEvent.php?eventId=$popularEventId">
+												<a href="detailJobVacancies.php?jobId=$popularBlogId">
 													<div class="inner">
 														<img src="images/posts/tabs-1.jpg" alt="post-title" />
 													</div>
 												</a>
 											</div>
 											<div class="details clearfix">
-												<h6 class="post-title my-0"><a href="detailEvent.php?eventId=$popularEventId">$popularEventTitle</a></h6>
+												<h6 class="post-title my-0"><a href="detailJobVacancies.php?jobId=$popularBlogId">$popularBlogTitle</a></h6>
 												<ul class="meta list-inline mt-1 mb-0">
-													<li class="list-inline-item">$popularEventDate</li>
+													<li class="list-inline-item">$popularBlogDate</li>
 												</ul>
 											</div>
 										</div>
@@ -292,11 +290,11 @@ $result4 = mysqli_fetch_all($data4);
 									<ul class="list">
 										<?php
 										$jumlah = $result3[0][0];
-										echo "<li><a href='listEvent.php'>ALL</a><span>($jumlah)</span></li>";
+										echo "<li><a href='listBlog.php'>ALL</a><span>($jumlah)</span></li>";
 										foreach ($result2 as $isi) {
 											$categoryName = $isi[1];
 											$jumlah = $isi[2];
-											echo "<li><a href='listEvent.php?category=$categoryName'>$categoryName</a><span>($jumlah)</span></li>";
+											echo "<li><a href='listBlog.php?category=$categoryName'>$categoryName</a><span>($jumlah)</span></li>";
 										}
 										?>
 
--- reader/listJobVacancies.php
+++ reader/listJobVacancies.php
@@ -1,28 +1,26 @@
 <?php
 include('../helper/getConnectionMsqli.php');
-require_once __DIR__ . '/../helper/cloudinary.php';
-require_once __DIR__ . '/../helper/hash.php';
 
 $conn = getConnectionMysqli();
 
 $getCategory = isset($_GET['category']) ? $_GET['category'] : null;
 
 if (is_null($getCategory)) {
-	$query = "SELECT tb_event.event_id, tb_event.event_title, tb_event.date_release, tb_event.image_url, tb_editor.username, tb_category_event.category_name FROM tb_event INNER JOIN tb_editor ON tb_event.editor_id = tb_editor.editor_id INNER JOIN  tb_category_event ON tb_category_event.category_id = tb_event.category_id";
+	$query = "SELECT tb_job_vacancies.vacancy_id, tb_job_vacancies.vacancy_title, tb_job_vacancies.date_release, tb_job_vacancies.image_url, tb_editor.username, tb_category_job_vacancy.category_name FROM tb_job_vacancies INNER JOIN tb_editor ON tb_job_vacancies.editor_id = tb_editor.editor_id INNER JOIN  tb_category_job_vacancy ON tb_category_job_vacancy.category_id = tb_job_vacancies.category_id";
 } else {
-	$query = "SELECT tb_event.event_id, tb_event.event_title, tb_event.date_release, tb_event.image_url, tb_editor.username, tb_category_event.category_name FROM tb_event INNER JOIN tb_editor ON tb_event.editor_id = tb_editor.editor_id INNER JOIN  tb_category_event ON tb_category_event.category_id = tb_event.category_id WHERE tb_category_event.category_name = '$getCategory'";
+	$query = "SELECT tb_job_vacancies.vacancy_id, tb_job_vacancies.vacancy_title, tb_job_vacancies.date_release, tb_job_vacancies.image_url, tb_editor.username, tb_category_job_vacancy.category_name FROM tb_job_vacancies INNER JOIN tb_editor ON tb_job_vacancies.editor_id = tb_editor.editor_id INNER JOIN  tb_category_job_vacancy ON tb_category_job_vacancy.category_id = tb_job_vacancies.category_id WHERE tb_category_job_vacancy.category_name = '$getCategory'";
 }
 
 $data = mysqli_query($conn, $query);
 $result = mysqli_fetch_all($data);
-$query2 = "SELECT tb_event.category_id, tb_category_event.category_name, COUNT(tb_event.category_id) AS jumlah_kategori FROM tb_category_event INNER JOIN tb_event ON tb_category_event.category_id = tb_event.category_id GROUP BY tb_event.category_id";
-$query3 = "SELECT COUNT(event_id) AS jumlah_event FROM tb_event";
+$query2 = "SELECT tb_job_vacancies.category_id, tb_category_job_vacancy.category_name, COUNT(tb_job_vacancies.category_id) AS jumlah_kategori FROM tb_category_job_vacancy INNER JOIN tb_job_vacancies ON tb_category_job_vacancy.category_id = tb_job_vacancies.category_id GROUP BY tb_job_vacancies.category_id";
+$query3 = "SELECT COUNT(vacancy_id) AS jumlah_event FROM tb_job_vacancies";
 $data2 = mysqli_query($conn, $query2);
 $result2 = mysqli_fetch_all($data2);
 $data3 = mysqli_query($conn, $query3);
 $result3 = mysqli_fetch_array($data3);
 
-$query4 = "SELECT event_id, event_title, date_release, views, image_url FROM tb_event ORDER BY views desc limit 3";
+$query4 = "SELECT vacancy_id, vacancy_title, date_release, views, logo FROM tb_job_vacancies ORDER BY views desc limit 3";
 $data4 = mysqli_query($conn, $query4);
 $result4 = mysqli_fetch_all($data4);
 ?>
@@ -132,7 +130,7 @@ $result4 = mysqli_fetch_all($data4);
 							<li class="nav-item">
 								<a class="nav-link" href="index.php">Home</a>
 							</li>
-							<li class="nav-item active">
+							<li class="nav-item">
 								<a class="nav-link" href="listEvent.php">Event</a>
 							</li>
 							<li class="nav-item">
@@ -141,7 +139,7 @@ $result4 = mysqli_fetch_all($data4);
 							<li class="nav-item">
 								<a class="nav-link" href="listMedia.php">Media</a>
 							</li>
-							<li class="nav-item">
+							<li class="nav-item active">
 								<a class="nav-link" href="listJobVacancies.php">Loker/Magang</a>
 							</li>
 						</ul>
@@ -154,11 +152,11 @@ $result4 = mysqli_fetch_all($data4);
 		<section class="page-header">
 			<div class="container-xl">
 				<div class="text-center">
-					<h1 class="mt-0 mb-2">Event</h1>
+					<h1 class="mt-0 mb-2">Loker/Magang</h1>
 					<nav aria-label="breadcrumb">
 						<ol class="breadcrumb justify-content-center mb-0">
 							<li class="breadcrumb-item"><a href="#">Home</a></li>
-							<li class="breadcrumb-item active" aria-current="page">Event</li>
+							<li class="breadcrumb-item active" aria-current="page">Loker/Magang</li>
 						</ol>
 					</nav>
 				</div>
@@ -177,10 +175,10 @@ $result4 = mysqli_fetch_all($data4);
 							<?php
 							if (mysqli_num_rows($data) > 0) {
 								foreach ($result as $isi) {
-									$eventId = $isi[0];
-									$eventTitle = $isi[1];
+									$jobId = $isi[0];
+									$jobTitle = $isi[1];
 									$dateRelease = $isi[2];
-									$image = getImageDefault(decryptPhotoProfile($isi[3]));
+									$image = $isi[3];
 									$editorUsername = $isi[4];
 									$categoryName = $isi[5];
 
@@ -192,16 +190,16 @@ $result4 = mysqli_fetch_all($data4);
 													<a href="category.html" class="category-badge position-absolute">$categoryName</a>
 													<a href="blog-single.html">
 														<div class="inner">
-															<img src="$image" alt="post-title" />
+															<img src="images/posts/post-md-2.jpg" alt="post-title" />
 														</div>
 													</a>
 												</div>
 												<div class="details">
 													<ul class="meta list-inline mb-0">
-														<li class="list-inline-item"><a href="#">$editorUsername</a></li>
+														<li class="list-inline-item"><a href="#"><img src="images/other/author-sm.png" class="author" alt="author" />$editorUsername</a></li>
 														<li class="list-inline-item">$dateRelease</li>
 													</ul>
-													<h5 class="post-title mb-3 mt-3"><a href="detailEvent.php?eventId=$eventId">$eventTitle</a></h5>
+													<h5 class="post-title mb-3 mt-3"><a href="detailJobVacancies.php?jobId=$jobId">$jobTitle</a></h5>
 												</div>
 												<div class="post-bottom clearfix d-flex align-items-center">
 													<div class="social-share me-auto">
@@ -242,9 +240,6 @@ $result4 = mysqli_fetch_all($data4);
 
 						<!-- sidebar -->
 						<div class="sidebar">
-							<!-- widget about -->
-
-
 							<!-- widget popular posts -->
 							<div class="widget rounded">
 								<div class="widget-header text-center">
@@ -255,23 +250,23 @@ $result4 = mysqli_fetch_all($data4);
 									<?php
 									$number = 1;
 									foreach ($result4 as $data) {
-										$popularEventId = $data[0];
-										$popularEventTitle = $data[1];
-										$popularEventDate = $data[2];
+										$popularJobId = $data[0];
+										$popularJobTitle = $data[1];
+										$popularJobDate = $data[2];
 										echo <<<Buat
 											<div class="post post-list-sm circle">
 											<div class="thumb circle">
 												<span class="number">$number</span>
-												<a href="detailEvent.php?eventId=$popularEventId">
+												<a href="detailJobVacancies.php?jobId=$popularJobId">
 													<div class="inner">
 														<img src="images/posts/tabs-1.jpg" alt="post-title" />
 													</div>
 												</a>
 											</div>
 											<div class="details clearfix">
-												<h6 class="post-title my-0"><a href="detailEvent.php?eventId=$popularEventId">$popularEventTitle</a></h6>
+												<h6 class="post-title my-0"><a href="detailJobVacancies.php?jobId=$popularJobId">$popularJobTitle</a></h6>
 												<ul class="meta list-inline mt-1 mb-0">
-													<li class="list-inline-item">$popularEventDate</li>
+													<li class="list-inline-item">$popularJobDate</li>
 												</ul>
 											</div>
 										</div>
@@ -292,11 +287,11 @@ $result4 = mysqli_fetch_all($data4);
 									<ul class="list">
 										<?php
 										$jumlah = $result3[0][0];
-										echo "<li><a href='listEvent.php'>ALL</a><span>($jumlah)</span></li>";
+										echo "<li><a href='listJobVacancies.php'>ALL</a><span>($jumlah)</span></li>";
 										foreach ($result2 as $isi) {
 											$categoryName = $isi[1];
 											$jumlah = $isi[2];
-											echo "<li><a href='listEvent.php?category=$categoryName'>$categoryName</a><span>($jumlah)</span></li>";
+											echo "<li><a href='listJobVacancies.php?category=$categoryName'>$categoryName</a><span>($jumlah)</span></li>";
 										}
 										?>
 
