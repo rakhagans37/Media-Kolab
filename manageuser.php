b313f4e628b533a4293db5b4ae281a13efa0f578
rakhagans37 <muhammadrakha3704@gmail.com>
2023-11-27 09:17:13 +0700

    Halaman Admin, hampir final

--- manageuser.php
+++ manageuser.php
@@ -59,24 +59,23 @@ $requestBannedUser = mysqli_query($conn, $sqlBannedUser);
 <html lang="en">
 
 <head>
-    <title>Portal - Bootstrap 5 Admin Dashboard Template For Developers</title>
+    <title>Ngampus.id - For Admin</title>
 
     <!-- Meta -->
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
-    <meta name="description" content="Portal - Bootstrap 5 Admin Dashboard Template For Developers">
+    <meta name="description" content="Ngampus.id - For Admin">
     <meta name="author" content="Xiaoying Riley at 3rd Wave Media">
-    <link rel="shortcut icon" href="favicon.ico">
+    <link rel="shortcut icon" href="favicon-image.ico">
 
     <!-- FontAwesome JS-->
     <script defer src="assets/plugins/fontawesome/js/all.min.js"></script>
 
     <!-- App CSS -->
-    <!-- <link id="theme-style" rel="stylesheet" href="assets/css/portal.css"> -->
-    <link id="theme-style" rel="stylesheet" href="assets\scss\portal.css">
-
+    <link id="theme-style" rel="stylesheet" href="assets/css/portal.css">
+    <!-- <link id="theme-style" rel="stylesheet" href="assets/scss/portal.css"> -->
 </head>
 
 <body class="app">
